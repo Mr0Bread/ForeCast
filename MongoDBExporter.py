9aa64571ace7991d93c5e90b4bbf602e343facbe
Mr0Bread <zimnikovvladislav@gmail.com>
2020-01-17 15:05:09 +0200

    another little update on MongoDBExporter.py

--- MongoDBExporter.py
+++ MongoDBExporter.py
@@ -1,9 +1,23 @@
 import pymongo
 from Request import get_data_doc
 
-my_client = pymongo.MongoClient(
-    "mongodb+srv://Mr0Bread:Elishka1Love@forecastcluster-ruxkg.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
-database = my_client['MeteoInfoTable']
+class MongoDBExporter:
+    def __init__(self):
+        self.my_client = pymongo.MongoClient(
+            "mongodb+srv://Mr0Bread:Elishka1Love@forecastcluster-ruxkg.gcp.mongodb.net/test?retryWrites=true&w=majority")
+        self.database = self.my_client['MeteoInfoTable']
 
-my_dict = get_data_doc()
+    def clear_collection(self, coll_name: str):
+        self.database.drop_collection(coll_name)
+
+    def clear_all_collections(self, coll_names: list):
+        for name in coll_names:
+            self.database.drop_collection(name)
+
+    def get_collection(self, coll_name: str):
+        return self.database[coll_name]
+
+    def fill_collection(self, coll_name: str, fill_data: list):
+        collection = self.get_collection(coll_name)
+        collection.insert()
